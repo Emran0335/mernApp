e88dea12420e45a635a13713e78acf4ef99da0d9
Emran Hossain <emranhossain0335@gmail.com>
2024-08-09 20:54:53 +0600

    dashboard is complete!

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 import PropertyForm from "../components/PropertyForm";
 import PropertyItem from "../components/PropertyItem";
 import { useDispatch, useSelector } from "react-redux";
